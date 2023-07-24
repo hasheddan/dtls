54ae034de53c45e199de720d720d3c6fe367e357
Daniel Mangum <georgedanielmangum@gmail.com>
2023-07-24 11:35:31 -0400

    only send gen
    Signed-off-by: Daniel Mangum <georgedanielmangum@gmail.com>

--- connection_id.go
+++ connection_id.go
@@ -6,7 +6,8 @@ package dtls
 import "crypto/rand"
 
 // RandomCIDGenerator is a random Connection ID generator where CID is the
-// specified size.
+// specified size. Specifying a size of 0 will indicate to peers that sending a
+// Connection ID is not necessary.
 func RandomCIDGenerator(size int) func() []byte {
 	return func() []byte {
 		cid := make([]byte, size)
@@ -14,5 +15,14 @@ func RandomCIDGenerator(size int) func() []byte {
 			panic(err) //nolint -- nonrecoverable
 		}
 		return cid
+	}
+}
+
+// OnlySendCIDGenerator enables sending Connection IDs negotiated with a peer,
+// but indicates to the peer that sending Connection IDs in return is not
+// necessary.
+func OnlySendCIDGenerator() func() []byte {
+	return func() []byte {
+		return nil
 	}
 }
--- connection_id_test.go
+++ connection_id_test.go
@@ -5,7 +5,7 @@ package dtls
 
 import "testing"
 
-func TestRandomConnectionID(t *testing.T) {
+func TestRandomConnectionIDGenerator(t *testing.T) {
 	cases := map[string]struct {
 		reason string
 		size   int
@@ -23,6 +23,23 @@ func TestRandomConnectionID(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			if cidLen := len(RandomCIDGenerator(tc.size)()); cidLen != tc.size {
 				t.Errorf("%s\nRandomCIDGenerator: expected CID length %d, but got %d.", tc.reason, tc.size, cidLen)
+			}
+		})
+	}
+}
+
+func TestOnlySendCIDGenerator(t *testing.T) {
+	cases := map[string]struct {
+		reason string
+	}{
+		"LengthMatch": {
+			reason: "CID length should always be zero.",
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			if cidLen := len(OnlySendCIDGenerator()()); cidLen != 0 {
+				t.Errorf("%s\nOnlySendCIDGenerator: expected CID length %d, but got %d.", tc.reason, 0, cidLen)
 			}
 		})
 	}
